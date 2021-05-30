655881e60830460cfd95140c51fe64e4ef22645f
ddhh83 <daniel.herzig@outlook.at>
2021-05-30 16:05:21 +0200

    adding version 2.9 to definition

--- my-hello.scm
+++ my-hello.scm
@@ -24,3 +24,23 @@
   mmand-line arguments, multiple languages, and so on.")
     (home-page "https://www.gnu.org/software/hello/")
     (license gpl3+)))
+
+(define-public my-hello-2.9
+  (package
+    (name "my-hello")
+    (version "2.9")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/hello/hello-" version
+                                  ".tar.gz"))
+              (sha256
+               (base32
+                "19qy37gkasc4csb1d3bdiz9snn8mir2p3aj0jgzmfv0r2hi7mfzc"))))
+    (build-system gnu-build-system)
+    (synopsis "Hello, GNU world: An example custom Guix package")
+    (description
+     "GNU Hello prints the message \"Hello, world!\" and then exits.  It
+  rves as an example of standard GNU coding practices.  As such, it supports
+  mmand-line arguments, multiple languages, and so on.")
+    (home-page "https://www.gnu.org/software/hello/")
+    (license gpl3+)))
