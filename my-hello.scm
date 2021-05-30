9d394a8b41574c7f016b22cdcb9ac6d398921e26
ddhh83 <daniel.herzig@outlook.at>
2021-05-30 16:20:04 +0200

    added definitions for versions 2.8 and 2.7

--- my-hello.scm
+++ my-hello.scm
@@ -44,3 +44,43 @@
   mmand-line arguments, multiple languages, and so on.")
     (home-page "https://www.gnu.org/software/hello/")
     (license gpl3+)))
+
+(define-public my-hello-2.8
+  (package
+    (name "my-hello")
+    (version "2.8")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/hello/hello-" version
+                                  ".tar.gz"))
+              (sha256
+               (base32
+                "0wqd8sjmxfskrflaxywc7gqw7sfawrfvdxd9skxawzfgyy0pzdz6"))))
+    (build-system gnu-build-system)
+    (synopsis "Hello, GNU world: An example custom Guix package")
+    (description
+     "GNU Hello prints the message \"Hello, world!\" and then exits.  It
+  rves as an example of standard GNU coding practices.  As such, it supports
+  mmand-line arguments, multiple languages, and so on.")
+    (home-page "https://www.gnu.org/software/hello/")
+    (license gpl3+)))
+
+(define-public my-hello-2.7
+  (package
+    (name "my-hello")
+    (version "2.7")
+    (source (origin
+              (method url-fetch)
+              (uri (string-append "mirror://gnu/hello/hello-" version
+                                  ".tar.gz"))
+              (sha256
+               (base32
+                "1h17p5lgg47lbr2cnp4qqkr0q0f0rpffyzmvs7bvc6vdrxdknngx"))))
+    (build-system gnu-build-system)
+    (synopsis "Hello, GNU world: An example custom Guix package")
+    (description
+     "GNU Hello prints the message \"Hello, world!\" and then exits.  It
+  rves as an example of standard GNU coding practices.  As such, it supports
+  mmand-line arguments, multiple languages, and so on.")
+    (home-page "https://www.gnu.org/software/hello/")
+    (license gpl3+)))
